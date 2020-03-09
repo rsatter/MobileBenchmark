59cd399c0884c9e26dcfd359bc66dc9a696b02a9
Rabi Satter <rabi@satter.org>
2020-03-09 15:23:02 -0700

    Created Database include poco model and data.

---
+++ src/Xamarin/XamarinFormsBenchmark/XamarinFormsBenchmark/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SQLite;
+
+namespace XamarinFormsBenchmark.Models
+{
+    public class Order
+    {
+        public string CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        public double Freight { get; set; }
+        [PrimaryKey]
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipRegion { get; set; }
+        public int ShipVia { get; set; }
+    }
+}
---
+++ src/Xamarin/XamarinFormsBenchmark/XamarinFormsBenchmark/Models/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SQLite;
+
+namespace XamarinFormsBenchmark.Models
+{
+    public class OrderDetail
+    {
+        public double Discount { get; set; }
+        [PrimaryKey]
+        public int Id { get; set; }
+        [Indexed]
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int  Quantity { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
